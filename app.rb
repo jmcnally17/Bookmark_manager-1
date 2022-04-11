88dd55e505ffd023794fc6894f9bcdd49a67ff23
David Ewan Campbell <ewan.david@gmail.com>
2022-04-11 15:12:30 +0100

    2.Setting Up Web Project - gems/rspec bundle install and first passing homepage feature test

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BookmarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'This is your bookmark manager'
+  end
+
+end
